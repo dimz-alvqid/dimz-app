d43ea564346c359cbaedc916cb1ae1d81b1c1206
dimz404 <dimasbagussaputra404@gmail.com>
2024-08-02 09:24:37 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" sizes="any" type="image/png" />
+        <link rel="icon" href="/favicon.jpg" type="image/jpg" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
