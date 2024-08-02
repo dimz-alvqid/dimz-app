e5ba00df74cda50a274d252aadc8e62cb74d4f0f
dimz404 <dimasbagussaputra404@gmail.com>
2024-08-02 10:12:01 +0700

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor felis a magna fermentum, vel malesuada lectus commodo.</p>
+    </div>
+  );
+}
