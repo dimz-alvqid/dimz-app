f0d86665eb729b465347aedcdefdbcc58c9cc236
dimz404 <dimasbagussaputra404@gmail.com>
2024-08-02 10:11:15 +0700

    Create page.tsx

---
+++ app/links/page.tsx
@@ -0,0 +1,13 @@
+export default function LinksPage() {
+  return (
+    <div>
+      <h1>Links</h1>
+      <ul>
+        <li><a href="https://youtube.com">YouTube</a></li>
+        <li><a href="https://tiktok.com">TikTok</a></li>
+        <li><a href="https://x.com">X (Twitter)</a></li>
+        <li><a href="https://instagram.com">Instagram</a></li>
+      </ul>
+    </div>
+  );
+}
