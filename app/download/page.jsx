4e886516cc159ba003131451943b52bf6ef30fc9
Alvin132QR <oa8929843@gmail.com>
2024-08-03 14:13:55 +0700

    Update page.jsx

--- app/download/page.jsx
+++ app/download/page.jsx
@@ -1,24 +1,7 @@
-import React from 'react';
-import linkData from './link.js';
-
 export default function Page() {
   return (
    <div class="table">
-      <thead>
-        <tr>
-          <th>NAME</th>
-        </tr>
-      </thead>
-      <tbody>
-        {linkData.map((array) => {
-          return (
-           <tr>
-             <td><a href={`/${array.id}`}>{array.name}</a></td>
-           </tr>
-          )
-        })
-        }
-      </tbody>
+     <p>Download Pages</p>
    </div>
   );
 }
