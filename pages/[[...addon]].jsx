1e8f553397f7aaf07689bf92a134d305b9463954
Alvin132QR <oa8929843@gmail.com>
2024-08-03 12:03:42 +0700

    Update [[...addon]].jsx

--- pages/[[...addon]].jsx
+++ pages/[[...addon]].jsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import FetchApi from './api';
 
-const AddonPage = () => {
+async function AddonPage() {
   const router = useRouter();
   const { addon } = router.query;
   
