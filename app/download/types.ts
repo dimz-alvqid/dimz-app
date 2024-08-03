a0f02ae5dbc930e91bc80c921a77ea20ebb834cc
dimz404 <dimasbagussaputra404@gmail.com>
2024-08-03 10:58:28 +0700

    Create types.ts

---
+++ app/download/types.ts
@@ -0,0 +1,10 @@
+export interface ContainerData {
+  name: string;
+  id: string;
+  showcaselink: string;
+  shortenerlink: string;
+  alternativelink?: string;
+  hidealternativelink?: boolean;
+  targetlink: string;
+  redirectlink: string;
+}
