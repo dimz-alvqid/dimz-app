661f8ec0e6135d338841e5c4ecbe2f1e3f45253e
Alvin132QR <oa8929843@gmail.com>
2024-08-03 13:50:54 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from '../lib/navigation';
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Dimz Project",
@@ -19,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Navigation />
-      <body className={inter.className}>{children}</body>
+      <body><main>{children}</main></body>
     </html>
   );
 }
