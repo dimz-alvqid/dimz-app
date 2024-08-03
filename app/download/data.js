50613b765564fe98439ad5a0e4eb7c53b57b585f
Alvin132QR <oa8929843@gmail.com>
2024-08-03 12:54:08 +0700

    https://dimzproject.my.id/download/advanced-container
    Fix

---
+++ app/download/data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    "name": "advanced container",
+    "id": "advanced-container",
+    "showcaselink": "https://youtube.com/...",
+    "shortenerlink": "https://linkvertise.com/...",
+    "alternativelink": "https://lootlabs.com/...",
+    "hidealternativelink": true,
+    "targetlink": "https://raw.githubusercontent.com/yourusername/yourrepo/main/yourfile.zip",
+    "redirectlink": "https://youtube.com/..."
+  }
+];
+
+export default data;
