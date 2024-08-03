4e1f3af76f7fe91004ff2cab66f409cda0a14ae4
Alvin132QR <oa8929843@gmail.com>
2024-08-03 12:16:00 +0700

    Create page.jsx

---
+++ app/download/[id]/page.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import data from '../data.js';
+
+export default function Page() {
+  const router = useRouter();
+  const { id } = router.query;
+  
+  const containerData = data.find((item) => item.id === id);
+
+  useEffect(() => {
+    if (containerData) {
+      window.location.href = containerData.targetlink;
+      setTimeout(() => {
+        window.location.href = containerData.redirectlink;
+      }, 5000); // Delay 5 detik sebelum redirect ke redirectlink
+    }
+  }, [containerData]);
+
+  if (!containerData) {
+    return <p>Container not found.</p>;
+  }
+
+  return <p>Preparing your download...</p>;
+}
