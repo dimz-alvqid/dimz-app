e41fcc6030d689eb7f30061af5dfd7aed9b69cba
Alvin132QR <oa8929843@gmail.com>
2024-08-03 13:55:58 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Navigation />
-      <body><main>{children}</main></body>
+      <body>
+        <Navigation />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
