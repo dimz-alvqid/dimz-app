f2373fab25ecd94ecec78be68b8c895d4c4bbc96
dimz404 <dimasbagussaputra404@gmail.com>
2024-08-03 13:03:41 +0700

    Create page.tsx

---
+++ app/download/[id]/[downloadid]/page.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import linkData from '../../link.json';
+
+export default function RedirectPage() {
+  const router = useRouter();
+  const { id, downloadid } = router.query;
+
+  useEffect(() => {
+    if (id && downloadid) {
+      const foundItem = linkData.find((item) => item.id === id && item.downloadid === downloadid);
+      if (foundItem) {
+        // Redirect to targetlink and then to redirectlink
+        window.location.href = foundItem.targetlink;
+        setTimeout(() => {
+          window.location.href = foundItem.redirectlink;
+        }, 1000); // Delay to allow the first redirect to happen
+      }
+    }
+  }, [id, downloadid]);
+
+  return <div>Redirecting...</div>;
+}
