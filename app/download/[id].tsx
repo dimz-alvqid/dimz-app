52c4b731356477a34cb4faa17bfe2b7132758f5c
dimz404 <dimasbagussaputra404@gmail.com>
2024-08-03 08:37:24 +0700

    Update [id].tsx

--- app/download/[id].tsx
+++ app/download/[id].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import data from '/app/_downloadLink.json';
+import data from '../_downloadLink.json';
 
 export default function ContainerPage() {
   const router = useRouter();
