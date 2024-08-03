f1ef3099427f2c7d5837772f45aebb7b27f1c15b
dimz404 <dimasbagussaputra404@gmail.com>
2024-08-03 08:19:51 +0700

    Update [downloadId].tsx

--- app/download/[id]/[downloadId].tsx
+++ app/download/[id]/[downloadId].tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import data from 'download/link.json';
+import data from './download/link.json';
 
 export default function DownloadPage() {
   const router = useRouter();
