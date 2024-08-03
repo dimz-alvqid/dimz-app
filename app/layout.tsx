47e5cd3f242a367b6b710ef6f5ed2f294e5226ac
Alvin132QR <oa8929843@gmail.com>
2024-08-03 13:38:50 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SSRProvider from 'react-bootstrap/SSRProvider';
 import Navigation from '../lib/navigation';
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <SSRProvider>
-        <Navigation />
-      </SSRProvider>
+      <Navigation />
       <body className={inter.className}>{children}</body>
     </html>
   );
