a6f541eda464e5fcef3488fed82e9c1519c3781c
Alvin132QR <oa8929843@gmail.com>
2024-08-03 17:10:13 +0700

    Update page.jsx

--- app/download/page.jsx
+++ app/download/page.jsx
@@ -2,23 +2,11 @@ import linkData from './link';
 export default function Page() {
   return (
     <div>
-     <table class="table">
-      <thead>
-       <tr>
-        <th scope="col">NAME</th>
-       </tr>
-      </thead>
-      <tbody>
-        {linkData.map((array) => {
-          return (
-           <tr>
-             <td><a href={`/${array.id}`}>{array.name}</a></td>
-           </tr>
-          )
-        })
-        }
-       </tbody>
-      </table>
+      <div class="card">
+        <div class="card-body">
+          <p>Download Pages!</p>
+        </div>
+      </div>
      </div>
   );
 }
