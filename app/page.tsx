39c563f1be85ffa169463515b1533d16c3c953a5
Alvin132QR <oa8929843@gmail.com>
2024-08-04 11:28:49 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,22 @@ export default function HomePage() {
         <li><Link href="/about">About</Link></li>
         <li><a href="/random">Random (Page Not Found)</a></li>
       </ul>
+      <div style={{ marginTop: "100px" }}></div>
+      <div class="container w-container">
+    <div class="welcome-container">
+    <div class="welcome-title">
+    <h1 class="heading">Welcome</h1>
+    </div>
+    <div class="testimonial-card-three">
+    <div class="dropdown-card-content">
+      <center>
+        <h1>Dimz Project.</h1>
+        <p>©2024 Dimz&AlvinQ-id</p>
+      </center>
+    </div>
+      </div>
+      </div>
+        </div>
     </div>
   );
 }
