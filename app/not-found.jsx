23480ff3226481a5ae692e791b8035e9bbfe63dc
Alvin132QR <oa8929843@gmail.com>
2024-08-04 12:59:56 +0700

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,5 +1,5 @@
 export default function NotFoundPage() {
   return (
-    <div class="mt-24 px-4"><center><div><img src="./patrick-lag.jpg" alt="patrick lag" class="mb-5 max-w-full h-auto"></div><h1 class="text-2xl font-bold">404 - Page Not Found</h1><div class="text-gray-600 dark:text-gray-300 mt-4">Hmmm, the page doesn't exist. Maybe I pasted the wrong link or are you trying something?</div><div class="mt-5"><a href="/"><button class="bg-green-600 text-white font-bold py-2 px-4 rounded hover:bg-green-700">Return to Home</button></a></div></center></div>
+    <div class="mt-24 px-4"><center><div><img src="./patrick-lag.jpg" alt="patrick lag" class="mb-5 max-w-full h-auto" /></div><h1 class="text-2xl font-bold">404 - Page Not Found</h1><div class="text-gray-600 dark:text-gray-300 mt-4">Hmmm, the page doesn't exist. Maybe I pasted the wrong link or are you trying something?</div><div class="mt-5"><a href="/"><button class="bg-green-600 text-white font-bold py-2 px-4 rounded hover:bg-green-700">Return to Home</button></a></div></center></div>
   );
 }
