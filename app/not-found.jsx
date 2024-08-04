8907a8a5439fd5340ac059fa2ee1d613db82318c
Alvin132QR <oa8929843@gmail.com>
2024-08-04 12:57:53 +0700

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,19 +1,5 @@
-"use client";
-import { Container, Row, Col, Card } from 'react-bootstrap';
-
 export default function NotFoundPage() {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Card>
-            <Card.Body>
-              <p>404 - Page Not Found!</p>
-              <a class="btn btn-primary" href="/">Back</a>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <div class="mt-24 px-4"><center><div><img src="./patrick-lag.jpg" alt="patrick lag" class="mb-5 max-w-full h-auto"></div><h1 class="text-2xl font-bold">404 - Page Not Found</h1><div class="text-gray-600 dark:text-gray-300 mt-4">Hmmm, the page doesn't exist. Maybe I pasted the wrong link or are you trying something?</div><div class="mt-5"><a href="/"><button class="bg-green-600 text-white font-bold py-2 px-4 rounded hover:bg-green-700">Return to Home</button></a></div></center></div>
   );
 }
