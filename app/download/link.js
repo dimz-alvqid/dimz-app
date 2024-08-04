53e38c6ed97440229c5553992c027968d60dc1d7
Alvin132QR <oa8929843@gmail.com>
2024-08-04 10:50:45 +0700

    Update link.js

--- app/download/link.js
+++ app/download/link.js
@@ -1,3 +1,5 @@
-import linkData from '@/data/link';
-export default linkData;
 import 'bootstrap/dist/css/bootstrap.min.css';
+import database from '@/data/link';
+
+const linkData = database;
+export default linkData;
